f8d08f2335199a14b09bd3d95cad6cd30f86d52a
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-24 20:07:27 -0400

    Make the elements for the about page
    Makes the elements for the about page and styles the text.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,25 @@ import "./About.css";
 export default function About() {
     return (
         <section id="about">
-
+            <div className="about-text-container">
+                <p className="about-text">
+                    The Dev Den is a place for computing students to hangout and work on projects
+                    together. We get together weekly, and people collaborate with each other on their
+                    personal projects. Have an idea for a project but you don't think you can handle 
+                    it alone? The Dev Den is the place for you. Want to gain experience working on a 
+                    project with others but you don't know of any group projects you can join? The 
+                    Dev Den is the place for you. Want to come hangout with other computing students 
+                    and brainstorm your next awesome project? The Dev Den. The club is open to all 
+                    Queen's computing students all year round, so anyone can join anytime and help 
+                    others with their projects or start their own. We also hld monthly informal 
+                    hackathons where our members can show off the projects they have been working on 
+                    and get to see everyone else's projects. The goal of the Dev Den is to provide a 
+                    casual environment for computing students to hangout, develop their skills and 
+                    expand their portfolios. The Dev Den creates an amazing opportunity for students 
+                    to learn from other students while working on projects that will shine on their 
+                    resumes.
+                </p>
+            </div>
         </section>
     )
 }
