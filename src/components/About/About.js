ef03a0de889225cb0310f7548f316a08043ad804
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-26 15:27:51 -0400

    Add dev den image and finish about page
    Finishes the styling for the about page and adds cartoon dev den image.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,25 +2,34 @@ import "./About.css";
 export default function About() {
     return (
         <section id="about">
-            <h2>What is the Dev Den?</h2>
-            <div className="about-text-container">
-                <p className="about-text">
-                    The Dev Den is a place for computing students to hangout and work on projects
-                    together. We get together weekly, and people collaborate with each other on their
-                    personal projects. Have an idea for a project but you don't think you can handle 
-                    it alone? The Dev Den is the place for you. Want to gain experience working on a 
-                    project with others but you don't know of any group projects you can join? The 
-                    Dev Den is the place for you. Want to come hangout with other computing students 
-                    and brainstorm your next awesome project? The Dev Den. The club is open to all 
-                    Queen's computing students all year round, so anyone can join anytime and help 
-                    others with their projects or start their own. We also hld monthly informal 
-                    hackathons where our members can show off the projects they have been working on 
-                    and get to see everyone else's projects. The goal of the Dev Den is to provide a 
-                    casual environment for computing students to hangout, develop their skills and 
-                    expand their portfolios. The Dev Den creates an amazing opportunity for students 
-                    to learn from other students while working on projects that will shine on their 
-                    resumes.
-                </p>
+            <div className="about-header">
+                <h2>What is the Dev Den?</h2>
+            </div>
+            <div className="about-content">
+                <img id="cartoon-dev-den" src="images/TheDevDen.png" alt="Cartoon Dev Den" width="2000px" height="2000px"/>
+                <div className="about-text-container">
+                    <p className="about-text">
+                        The Dev Den is a place for computing students to hangout and work on projects
+                        together. We get together weekly, and people collaborate with each other on their
+                        personal projects. Have an idea for a project but you don't think you can handle 
+                        it alone? The Dev Den is the place for you. Want to gain experience working on a 
+                        project with others but you don't know of any group projects you can join? The 
+                        Dev Den is the place for you. Want to come hangout with other computing students 
+                        and brainstorm your next awesome project? The Dev Den. 
+                    </p>
+                </div>
+            </div>
+            <div className="about-text-container about-text-2">
+            <p className="about-text">
+                The club is open to all Queen's computing students all year round, so anyone can join 
+                anytime and help others with their projects or start their own. We also hold monthly 
+                informal hackathons where our members can show off the projects they have been working on 
+                and get to see everyone else's projects. The goal of the Dev Den is to provide a 
+                casual environment for computing students to hangout, develop their skills and 
+                expand their portfolios. The Dev Den creates an amazing opportunity for students 
+                to learn from other students while working on projects that will shine on their 
+                resumes.
+            </p>
             </div>
         </section>
     )
