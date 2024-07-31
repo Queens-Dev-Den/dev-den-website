6633ff1ae0ef642cb8b35db8b5210b1b7d5895dc
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-31 19:27:06 -0400

    Added title to team section

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,7 +2,9 @@ import "./Team.css";
 export default function Team() {
     return (
         <section id="team">
-
+            <div className="team-header">
+                <h2>Meet Our Team</h2>
+            </div>
         </section>
     )
 }
