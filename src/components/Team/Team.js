f08cd09cdc97ae119e93076b99d03c6d95588652
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-31 20:01:47 -0400

    Laid out all the parts for team member cards
    Made a file which contains data for each team member and maps
    information and picture of each team member into the correct component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,23 @@
 import "./Team.css";
+import { TeamData } from './TeamData.js';
 export default function Team() {
     return (
         <section id="team">
             <div className="team-header">
                 <h2>Meet Our Team</h2>
+            </div>
+            <div className="team-card-section">
+                {TeamData.map((d) => (
+                    <div className="team-card">
+                        <div className="team-card-img">
+                            <img src={d.img} alt="Team Member"/>
+                        </div>
+                        <div className="team-card-text">
+                            <p className="team-member-name">{d.name}</p>
+                            <p className="team-member-position">{d.position}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     )
---
+++ src/components/Team/TeamData.js
@@ -0,0 +1,17 @@
+export const TeamData = [
+    {
+        name: 'Blank Name',
+        position: 'Blank Position',
+        img: 'team-images/zachary-gazaille.JPG'
+    },
+    {
+        name: 'Blank Name',
+        position: 'Blank Position',
+        img: 'team-images/zachary-gazaille.JPG'
+    },
+    {
+        name: 'Blank Name',
+        position: 'Blank Position',
+        img: 'team-images/zachary-gazaille.JPG'
+    }
+]
