efbdd42555a4a775ae83175952b642281463ae65
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-31 21:49:09 -0400

    Add and style react slick carousel
    Adds react slick carousel to the team member cards to scroll through the
    team members and adds custom arrows.

--- src/components/Team/TeamData.js
+++ src/components/Team/TeamData.js
@@ -1,16 +1,21 @@
 export const TeamData = [
     {
-        name: 'Blank Name',
+        name: 'Blank Name 1',
         position: 'Blank Position',
         img: 'team-images/zachary-gazaille.JPG'
     },
     {
-        name: 'Blank Name',
+        name: 'Blank Name 2',
         position: 'Blank Position',
         img: 'team-images/zachary-gazaille.JPG'
     },
     {
-        name: 'Blank Name',
+        name: 'Blank Name 3',
+        position: 'Blank Position',
+        img: 'team-images/zachary-gazaille.JPG'
+    },
+    {
+        name: 'Blank Name 4',
         position: 'Blank Position',
         img: 'team-images/zachary-gazaille.JPG'
     }
